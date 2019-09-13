97c6503c550a079bd1991bf3aecb13f8e1231bc3
Naveen-Bhaskar <52439258+Naveen-Bhaskar@users.noreply.github.com>
2019-09-13 12:10:05 +0530

    Add files via upload

---
+++ bckend/src/main/java/com/webpage/dao/Orderdao.java
@@ -0,0 +1,12 @@
+package com.webpage.dao;
+
+
+import com.webpage.model.Orderdetail;
+
+
+public interface Orderdao
+{
+public boolean paymentProcess(Orderdetail orderdetail);
+public boolean updateCartItemStatus(String username,int orderId);
+
+}
